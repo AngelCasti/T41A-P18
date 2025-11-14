fc3359c06319b7bbf53862259577b1f8880a26a9
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-13 21:43:46 -0600

    Create 03_query_data.sql

---
+++ 03_query_data.sql
@@ -0,0 +1,30 @@
+--ARRAYS
+SELECT * FROM productos WHERE 'tecnología' = ANY(etiquetas);
+
+--CTE
+WITH RECURSIVE red_jefes AS (
+    SELECT id, nombre, jefe_id FROM empleados WHERE nombre = 'David'
+    UNION ALL
+    SELECT e.id, e.nombre, e.jefe_id
+    FROM empleados e
+    INNER JOIN red_jefes r ON e.jefe_id = r.id
+)
+
+SELECT * FROM red_jefes;
+
+--Ejercicios Recomendados
+WITH RECURSIVE red_rutas AS (
+    SELECT id, nombre, ruta_id
+    FROM ciudades
+    WHERE nombre = 'Amsterdam'
+    UNION ALL
+    SELECT c.id, c.nombre, c.ruta_id
+    FROM ciudades c
+    JOIN red_rutas r
+      ON r.id = ANY(c.ruta_id)
+)
+
+SELECT DISTINCT * FROM red_rutas;
+
+
+
