9e866400baed05597e80a71b31a6b84fbeb12645
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-13 22:42:21 -0600

    Update test_18.py

--- test/test_18.py
+++ test/test_18.py
@@ -39,13 +39,30 @@ def test_consulta_productos_array_tecnologia(db_conn):
 
 def test_funcion_obtener_subordinados_de_jefe_2(db_conn):
     
-    query = "SELECT * FROM red_jefes;"
+    query = """WITH RECURSIVE red_jefes AS (
+        SELECT id, nombre, jefe_id FROM empleados WHERE id = 2
+        UNION ALL
+        SELECT e.id, e.nombre, e.jefe_id
+        FROM empleados e
+        INNER JOIN red_jefes r ON e.jefe_id = r.id
+    )
+    SELECT * FROM red_jefes;"""
     result = run_query(db_conn, query)
     assert len(result) > 0
 
 def test_funcion_obtener_ciudades_alcanzables(db_conn):
     query = """
-    SELECT DISTINCT * FROM red_rutas;
+    WITH RECURSIVE red_rutas AS (
+        SELECT id, nombre, ruta_id, ARRAY[id] AS visitados
+        FROM ciudades
+        WHERE nombre = 'Amsterdam'
+        UNION ALL
+        SELECT c.id, c.nombre, c.ruta_id, r.visitados || c.id
+        FROM ciudades c
+        JOIN red_rutas r ON c.id = ANY(r.ruta_id)
+        WHERE NOT c.id = ANY(r.visitados)
+    )
+    SELECT DISTINCT id, nombre, ruta_id FROM red_rutas;
     """
     result = run_query(db_conn, query)
     assert len(result) > 0
